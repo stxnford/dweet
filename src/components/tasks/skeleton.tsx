c68bd12e6dc4ac7661a2f831f3ebc46832dc11bd
stanfordnqobile <smendamenda@gmail.com>
2025-09-30 11:00:29 +0200

    added typing for formik forms

--- src/components/tasks/skeleton.tsx
+++ src/components/tasks/skeleton.tsx
@@ -4,7 +4,7 @@ import { Flex, Grid, Skeleton, SkeletonCircle } from '@chakra-ui/react';
 
 const SkeletonComponent = () => {
   return (
-    <Grid mt={4} templateColumns={'3fr 1fr 1fr 1fr'}>
+    <Grid mt={6} templateColumns={'3fr 1fr 1fr 1fr'}>
       <Flex align={'center'} gap={2}>
         <SkeletonCircle size={'4'} />
         <Flex direction={'column'} w={'full'} gap={2}>
@@ -14,6 +14,7 @@ const SkeletonComponent = () => {
             <Skeleton width={'35%'} height={2} />
             <Skeleton width={'25%'} height={2} />
           </Flex>
+          <Skeleton width={'45%'} height={2} />
         </Flex>
       </Flex>
       <Skeleton width={'60%'} height={2} />
